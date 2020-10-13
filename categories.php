66b4e35b4bb44f556861a06b92e2a45e659fd11e
Abhishek Anand <64414279+MildMitthu@users.noreply.github.com>
2020-10-13 10:01:20 +0530

    Add files via upload

---
+++ categories.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Templet File Doc Comment
+ * 
+ * PHP version /
+ * 
+ * @category Tenplete_Class
+ * @package  Templete_Class
+ * @author   Author <author@domain.com>
+ * @license  http://opensource.org/MIT MIT License
+ * @link     http://localhost/
+ */
+echo "categories";
+?>
